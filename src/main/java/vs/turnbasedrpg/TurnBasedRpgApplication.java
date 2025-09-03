7629f9f394869944a5678bbe2f4055c68eb00160
Vasilis Souvatzis <vasouv@gmail.com>
2025-09-03 22:21:30 +0300

    project init

---
+++ src/main/java/vs/turnbasedrpg/TurnBasedRpgApplication.java
@@ -0,0 +1,13 @@
+package vs.turnbasedrpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnBasedRpgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurnBasedRpgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vs/turnbasedrpg/TurnBasedRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package vs.turnbasedrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnBasedRpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
