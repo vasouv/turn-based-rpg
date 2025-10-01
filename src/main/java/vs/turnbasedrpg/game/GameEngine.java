6a0b2ba3200afee249b50aa2b4f116d7735d7c99
Vasilis Souvatzis <vasouv@gmail.com>
2025-10-02 00:04:29 +0300

    attack functionality

--- src/main/java/vs/turnbasedrpg/game/GameEngine.java
+++ src/main/java/vs/turnbasedrpg/game/GameEngine.java
@@ -2,6 +2,8 @@ package vs.turnbasedrpg.game;
 
 import vs.turnbasedrpg.entities.Player;
 
+import java.util.Objects;
+
 public class GameEngine {
 
     private final Player hero;
@@ -12,8 +14,8 @@ public class GameEngine {
         this.enemy = enemy;
     }
 
-    public static GameEngine initialize(Player hero) {
-        return new GameEngine(hero, null);
+    public static GameEngine initialize(Player hero, Player enemy) {
+        return new GameEngine(hero, enemy);
     }
 
     public Player getHero() {
@@ -22,6 +24,26 @@ public class GameEngine {
 
     public Player getEnemy() {
         return enemy;
+    }
+
+    public boolean gameInitialized() {
+        return Objects.nonNull(hero) && Objects.nonNull(enemy);
+    }
+
+    /**
+     * <h1>Attack phase</h1>
+     * <p>First, the raw damage is calculated. It's the attacker's attack value minus the defender's defense. It's compared to 0
+     * and the highest value is kept, this way there can be no negative damage if the attack is lower than the defense.</p>
+     * <p>The actual damage is the comparison between the raw damage and the defender's health. The minimum value is kept so
+     * that the defender's health is not negative.</p>
+     * @param attacker
+     * @param defender
+     */
+    public void attack(Player attacker, Player defender) {
+        int raw = Math.max(0, attacker.getAttack() - defender.getDefense());
+        int damage = Math.min(raw, defender.getHealth());
+        defender.setHealth(defender.getHealth() - damage);
+        System.out.printf("%s hits %s for %d DMG (%d HP left).%n", attacker.getName(), defender.getName(), damage, defender.getHealth());
     }
 
 }
--- src/test/java/vs/turnbasedrpg/game/GameEngineTest.java
+++ src/test/java/vs/turnbasedrpg/game/GameEngineTest.java
@@ -1,21 +1,79 @@
 package vs.turnbasedrpg.game;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import vs.turnbasedrpg.entities.Enemy;
 import vs.turnbasedrpg.entities.Hero;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GameEngineTest {
 
+    GameEngine gameEngine;
+
+    @BeforeEach
+    void setUp() {
+        var vasouv = new Hero("vasouv", 100, 20, 5);
+        var enemy = new Enemy("enemy", 80, 15, 3);
+
+        gameEngine = GameEngine.initialize(vasouv, enemy);
+    }
+
     @Test
-    @DisplayName("Initialize game engine with Hero")
+    @DisplayName("Initialize game engine with Hero and Enemy")
     void initializeWithHero() {
-        var vasouv = new Hero("vasouv", 15, 15, 15);
+        assertThat(gameEngine).isNotNull();
+        assertThat(gameEngine.getHero()).isNotNull();
+        assertThat(gameEngine.getEnemy()).isNotNull();
+    }
 
-        GameEngine gameEngine = GameEngine.initialize(vasouv);
+    @Test
+    @DisplayName("Hero attacks for 17 DMG - enemy health is 63")
+    void heroAttacks() {
+        gameEngine.attack(gameEngine.getHero(), gameEngine.getEnemy());
 
-        Assertions.assertThat(gameEngine).isNotNull();
-        Assertions.assertThat(gameEngine.getHero()).isNotNull();
-        Assertions.assertThat(gameEngine.getEnemy()).isNull();
+        assertThat(gameEngine.getEnemy().getHealth()).isEqualTo(63);
+    }
+
+    @Test
+    @DisplayName("Enemy attacks for 10 DMG - hero health is 90")
+    void enemyAttacks() {
+        gameEngine.attack(gameEngine.getEnemy(), gameEngine.getHero());
+
+        assertThat(gameEngine.getHero().getHealth()).isEqualTo(90);
+    }
+
+    @Test
+    @DisplayName("Hero and enemy attack each other once - both are alive")
+    void attackEachOtherOnce() {
+        gameEngine.attack(gameEngine.getHero(), gameEngine.getEnemy());
+        gameEngine.attack(gameEngine.getEnemy(), gameEngine.getHero());
+
+        assertThat(gameEngine.getHero().isAlive()).isTrue();
+        assertThat(gameEngine.getEnemy().isAlive()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Hero and enemy attack each other twice - both are alive")
+    void attackEachOtherTwice() {
+        gameEngine.attack(gameEngine.getHero(), gameEngine.getEnemy());
+        gameEngine.attack(gameEngine.getEnemy(), gameEngine.getHero());
+        gameEngine.attack(gameEngine.getHero(), gameEngine.getEnemy());
+        gameEngine.attack(gameEngine.getEnemy(), gameEngine.getHero());
+
+        assertThat(gameEngine.getHero().isAlive()).isTrue();
+        assertThat(gameEngine.getEnemy().isAlive()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Hero attacks 7 times - enemy is dead")
+    void enemyDies() {
+        for (int i = 0; i < 7; i++) {
+            gameEngine.attack(gameEngine.getHero(), gameEngine.getEnemy());
+        }
+        assertThat(gameEngine.getHero().isAlive()).isTrue();
+        assertThat(gameEngine.getEnemy().isDead()).isTrue();
+        assertThat(gameEngine.getEnemy().getHealth()).isEqualTo(0);
     }
 }
